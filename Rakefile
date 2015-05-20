5d4063050afe9ba64548d981dfd5b4c60ef2a264
Joel Segerlind <joel@burtcorp.com>
2015-05-20 07:20:03 +0200

    MVP

--- Rakefile
+++ Rakefile
@@ -10,13 +10,20 @@ namespace :install do
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular/angular.js',
       'bower_components/d3/d3.js',
+      'bower_components/ng-tags-input/ng-tags-input.js',
     ]
     %x[#{File.expand_path('..', __FILE__)}/node_modules/uglify-js/bin/uglifyjs #{dependencies.join(' ')} --compress --mangle --output public/js/lib.js]
   end
 
   desc 'Copy CSS dependencies into public/css/'
   task :css => [:bower_components] do
-    FileUtils.cp('bower_components/bootstrap/dist/css/bootstrap.css', 'public/css/bootstrap.css')
+    [
+      'bower_components/bootstrap/dist/css/bootstrap.css',
+      'bower_components/ng-tags-input/ng-tags-input.css',
+      'bower_components/ng-tags-input/ng-tags-input.bootstrap.css',
+    ].each do |stylesheet|
+      FileUtils.cp(stylesheet, "public/css/#{File.basename(stylesheet)}")
+    end
   end
 
   task :node_packages do
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 (function() {
-    var module = angular.module('names', []);
+    var module = angular.module('names', ['ngTagsInput']);
 
     // module.filter('transform', function() {
     //     return function(object, definition) {
@@ -27,7 +27,7 @@
                             return response.data.data.reduce(function(acc, curr) {
                                 var gender = curr.key[2];
                                 var entry = acc[gender] || {};
-                                entry[parseDate(curr.key[3])] = parseInt(curr.values[0])
+                                entry[curr.key[3]] = parseInt(curr.values[0])
                                 acc[gender] = entry;
                                 return acc;
                             }, {});
@@ -38,23 +38,27 @@
             names: (function() {
                 var url = "http://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0001/BE0001T05AR";
                 return {
-                    data: function() {
+                    data: function(filterValues) {
                         return $http({
                             "url": url,
-                            "data": {"query":[{"code":"Tilltalsnamn","selection":{"filter":"vs:Pojkar100","values":["1Albin","1Alex"]}},{"code":"ContentsCode","selection":{"filter":"item","values":["BE0001AJ"]}}],"response":{"format":"json"}},
+                            "data": {"query":[{"code":"Tilltalsnamn","selection":{"filter":"item","values":filterValues}},{"code":"ContentsCode","selection":{"filter":"item","values":["BE0001AJ"]}}],"response":{"format":"json"}},
                             "headers": {"Content-Type":"text/plain"},
                             "method": "POST"
                         }).then(function(response) {
-                            var names = response.data.data.reduce(function(acc, curr) {
-                                var id = curr.key[0];
-                                var entry = acc[id] || [];
-                                entry.push({date: parseDate(curr.key[1]), count: parseInt(curr.values[0])})
-                                acc[id] = entry;
+                            var columns = response.data.columns;
+                            var data = response.data.data.reduce(function(acc, curr) {
+                                var key = curr.key.reduce(function(key, value, i){
+                                    key[columns[i].code] = value;
+                                    return key;
+                                }, {});
+                                
+                                var value = curr.values[0];
+                                if ( value != '..' ) {
+                                    acc.push({key: key, value: parseInt(value)});
+                                }
                                 return acc;
-                            }, {});
-                            var res = [];
-                            for ( var id in names ) { res.push({id: id, entries: names[id]}) }
-                            return res;
+                            }, []);
+                            return data;
                         });
                     },
                     names: function() {
@@ -63,7 +67,12 @@
                             "method": "GET"
                         }).then(function(response) {
                             var tilltalsnamn = response.data.variables.reduce(function(prev, curr) { return prev || (curr.code == "Tilltalsnamn" && curr) }, false);
-                            return tilltalsnamn.values.reduce(function(prev, curr, i) { prev[curr] = tilltalsnamn.valueTexts[i]; return prev }, {});
+                            return tilltalsnamn.values.reduce(function(prev, curr, i) {
+                                if (curr[1] != '0' ) {
+                                    prev[curr] = tilltalsnamn.valueTexts[i]
+                                }
+                                return prev
+                            }, {});
                         });
                     }
                 }
@@ -72,47 +81,88 @@
     }]);
 
     module.controller("ApplicationController", ["$scope", "$http", '$q', '$timeout', 'scb', function($scope, $http, $q, $timeout, scb) {
-        scb.names.data().then(function(data){
-            scb.names.names().then(function(names){
-                scb.births.data().then(function(births){
-                    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-                        width = 960 - margin.left - margin.right,
-                        height = 500 - margin.top - margin.bottom;
-                    var x = d3.time.scale().range([0, width]);
-                    var y = d3.scale.linear().range([height, 0]).domain([0, 0.1]);
-                    var xAxis = d3.svg.axis().scale(x).orient("bottom");
-                    var line = d3.svg.line().x(function(d) { return x(d.date); }).y(function(d) { return y(d.ratio); });
-                    var svg = d3.select("body").append("svg")
-                        .attr("width", width + margin.left + margin.right)
-                        .attr("height", height + margin.top + margin.bottom)
-                        .append("g")
-                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-                    var minDate, maxDate, minRatio, maxRatio;
-                    data = data.map(function(d) {
-                        d.entries.forEach(function(e){
-                            e.ratio = e.count / births[d.id[0]][e.date];
-                            if ( typeof minRatio == "undefined" || minRatio > e.ratio ) { minRatio = e.ratio }
-                            if ( typeof maxRatio == "undefined" || maxRatio < e.ratio ) { maxRatio = e.ratio }
-                            if ( typeof minDate == "undefined" || minDate > e.date ) { minDate = e.date }
-                            if ( typeof maxDate == "undefined" || maxDate < e.date ) { maxDate = e.date }
-                        })
-                        return d;
-                    })
-                    x.domain([minDate, maxDate]);
-                    y.domain([minRatio, maxRatio]);
-                    svg.append("g")
-                        .attr("class", "x axis")
-                        .attr("transform", "translate(0," + height + ")")
-                        .call(xAxis);
-                    data.forEach(function(entry) {
-                        svg.append("path")
-                            .datum(entry.entries)
-                            .attr("class", "line line-1")
-                            .attr("title", names[entry.id])
-                            .attr("d", line);
+        $scope.filterValues = []
+        $scope.loadNames = function(query) {
+            return scb.names.names().then(function(names){
+                var matches = [];
+                for ( var id in names ) {
+                    var name = names[id];
+                    if ( name.toLowerCase().indexOf(query.toLowerCase()) > -1  ) {
+                        matches.push({id: id, name: name});
+                    }
+                }
+                return matches;
+            });
+        };
+        $scope.$watch('filterValues.length', function(size) {
+            scb.names.data($scope.filterValues.map(function(item){return item.id})).then(function(data){
+                scb.names.names().then(function(names){
+                    scb.births.data().then(function(births){
+                        var margin = {top: 20, right: 100, bottom: 30, left: 50},
+                            width = 960 - margin.left - margin.right,
+                            height = 500 - margin.top - margin.bottom;
+                        var x = d3.time.scale().range([0, width]);
+                        var y = d3.scale.linear().range([height, 0]).domain([0, 0.1]);
+                        var xAxis = d3.svg.axis().scale(x).orient("bottom");
+                        var line = d3.svg.line().x(function(d) { return x(d.date); }).y(function(d) { return y(d.ratio); });
+                        d3.selectAll(".graph").remove()
+                        var svg = d3.select("svg")
+                            .attr("width", width + margin.left + margin.right)
+                            .attr("height", height + margin.top + margin.bottom)
+                            .append("g")
+                            .attr("class", "graph")
+                            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                        var minDate, maxDate, minRatio, maxRatio;
+                        var parseDate = d3.time.format("%Y").parse;
+                        var series = data.reduce(function(series, d) {
+                            var year = d.key["Tid"]
+                            var id = d.key["Tilltalsnamn"];
+                            var serie = series[id] || [];
+                            var entry = {date: parseDate(year), ratio: d.value / births[id[0]][year]};
+                            serie.push(entry);
+                            if ( typeof minRatio == "undefined" || minRatio > entry.ratio ) { minRatio = entry.ratio }
+                            if ( typeof maxRatio == "undefined" || maxRatio < entry.ratio ) { maxRatio = entry.ratio }
+                            if ( typeof minDate == "undefined" || minDate > entry.date ) { minDate = entry.date }
+                            if ( typeof maxDate == "undefined" || maxDate < entry.date ) { maxDate = entry.date }
+                            series[id] = serie;
+                            return series;
+                        }, {})
+                        x.domain([minDate, maxDate]);
+                        y.domain([minRatio, maxRatio]);
+                        svg.append("g")
+                            .attr("class", "x axis")
+                            .attr("transform", "translate(0," + height + ")")
+                            .call(xAxis);
+
+                        var color = d3.scale.category10().domain(d3.keys(data));
+
+                        var slice = svg.selectAll(".slice")
+                            .data(d3.entries(series))
+                            .enter().append("g")
+                            .attr("class", "slice");
+
+                        slice.append("path")
+                            .attr("class", "line")
+                            .attr("d", function(d) { return line(d.value); })
+                            .style("stroke", function(d) { return color(d.key); });
+
+                        slice.append("text")
+                            .datum(function(d) { return {name: d.key, value: d.value[d.value.length - 1]}; })
+                            .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.ratio) + ")"; })
+                            .attr("x", 3)
+                            .attr("dy", ".35em")
+                            .text(function(d) { return names[d.name]; });
+
+                        // d3.entries(series).forEach(function(entry) {
+                        //     svg.append("path")
+                        //         .datum(entry.value)
+                        //         .attr("class", "line line-1")
+                        //         .attr("title", names[entry.key])
+                        //         .attr("d", line);
+                        // })
                     })
                 })
-            })
+            });
         });
     }]);
 })();
