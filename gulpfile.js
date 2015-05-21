b236f2cde9aaec90fe82b56a32779ddf3ae8c7d1
Joel Segerlind <joel@burtcorp.com>
2015-05-21 20:47:14 +0200

    Indentation again

--- gulpfile.js
+++ gulpfile.js
@@ -6,39 +6,39 @@ var source = require("vinyl-source-stream");
 var buffer = require("vinyl-buffer");
 
 gulp.task("jshint", function () {
-    return gulp.src(pkg.config.paths.main)
-        .pipe(plug.jshint())
-        .pipe(plug.jshint.reporter("jshint-stylish"));
+  return gulp.src(pkg.config.paths.main)
+    .pipe(plug.jshint())
+    .pipe(plug.jshint.reporter("jshint-stylish"));
 });
 
 gulp.task("views", function(){
-    return gulp.src(pkg.config.paths.views)
-        .pipe(gulp.dest(pkg.config.paths.public));
+  return gulp.src(pkg.config.paths.views)
+    .pipe(gulp.dest(pkg.config.paths.public));
 });
 
 gulp.task("stylesheets", function(){
-    return gulp.src([
-        "node_modules/bootstrap/dist/css/bootstrap.css",
-        "node_modules/ng-tags-input/build/ng-tags-input.css",
-        "node_modules/ng-tags-input/build/ng-tags-input.bootstrap.css",
-        pkg.config.paths.stylesheets
-    ])
-        .pipe(plug.size({showFiles: true}))
-        .pipe(plug.concatCss("main.css"))
-        .pipe(gulp.dest(pkg.config.paths.public + "/css"));
+  return gulp.src([
+    "node_modules/bootstrap/dist/css/bootstrap.css",
+    "node_modules/ng-tags-input/build/ng-tags-input.css",
+    "node_modules/ng-tags-input/build/ng-tags-input.bootstrap.css",
+    pkg.config.paths.stylesheets
+  ])
+    .pipe(plug.size({showFiles: true}))
+    .pipe(plug.concatCss("main.css"))
+    .pipe(gulp.dest(pkg.config.paths.public + "/css"));
 });
 
 gulp.task('scripts', ["jshint"], function() {
-    return browserify({entries: pkg.config.paths.main, debug: true}).bundle()
-        .pipe(source(pkg.name + ".js"))
-        .pipe(buffer())
-        .pipe(plug.size({showFiles: true}))
-        .pipe(plug.sourcemaps.init({loadMaps: true}))
-        .pipe(plug.uglify())
-        .pipe(plug.sourcemaps.write('./'))
-        .pipe(plug.rename(function(path) { if (path.extname == '.js') path.extname = '.min.js'; }))
-        .pipe(plug.size({showFiles: true}))
-        .pipe(gulp.dest(pkg.config.paths.public + "/js"))
+  return browserify({entries: pkg.config.paths.main, debug: true}).bundle()
+    .pipe(source(pkg.name + ".js"))
+    .pipe(buffer())
+    .pipe(plug.size({showFiles: true}))
+    .pipe(plug.sourcemaps.init({loadMaps: true}))
+    .pipe(plug.uglify())
+    .pipe(plug.sourcemaps.write('./'))
+    .pipe(plug.rename(function(path) { if (path.extname == '.js') path.extname = '.min.js'; }))
+    .pipe(plug.size({showFiles: true}))
+    .pipe(gulp.dest(pkg.config.paths.public + "/js"))
 });
 
 gulp.task('watch', function() {
